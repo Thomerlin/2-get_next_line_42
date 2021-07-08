feb3a630c0b80836bbf147ac4721e323e9eba607
Thomerlin <thomazzribeiro@hotmail.com>
2021-07-08 05:31:17 -0300

    poha8

--- get_next_line.h
+++ get_next_line.h
@@ -9,5 +9,9 @@ size_t	ft_strlen(const char *s, const char c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s);
 char	*ft_strjoin(char *s1, char *s2);
+char    *write_beginning(char *buf);
+char    *write_end(char **line, char *buf, int *end);
+int     read_buf(int fd, int *result, char **line, char **buf);
+int     process_end(char **line, char **end, int *is_end);
 
 #endif
