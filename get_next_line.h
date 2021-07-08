19256716dbb4f37e2440dd6a0dba34549747cd5b
Thomerlin <thomazzribeiro@hotmail.com>
2021-07-08 03:22:44 -0300

    pao

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# define FD_SIZE 4096
+
+typedef enum e_status
+{
+	ERROR = -1,
+	END_OF_FILE,
+	NEWLINE,
+}	t_status;
+
+int		get_next_line(int fd, char **line);
+
+size_t	ft_strlen(const char *str);
+
+char	*ft_strdup(const char *str);
+
+char	*ft_strjoin(const char *s1, const char *s2);
+
+char	*ft_strchr(const char *s, int c);
+
+char	*linedup(char *save, size_t end);
+
+#endif
