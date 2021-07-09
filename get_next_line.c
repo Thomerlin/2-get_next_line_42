f79afe10ea664ad825b9c0ee8e8b87f14c566789
Thomerlin <thomazzribeiro@hotmail.com>
2021-07-09 01:06:36 -0300

    again25

--- get_next_line.c
+++ get_next_line.c
@@ -97,8 +97,7 @@ int	get_next_line(int fd, char **line)
 	char		*bf;
 	int			res;
 
-	*line = malloc(sizeof(char));
-	if (fd < 0 || BUFFER_SIZE < 1 || line == NULL || *line == NULL)
+	if (fd < 0 || BUFFER_SIZE < 1 || !line || !*line = malloc(sizeof(char)))
 		return (-1);
 	**line = '\0';
 	if (is_end != '\0')
