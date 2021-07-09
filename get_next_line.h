a484ec35223ecffc54158cb4d676e470498e4e2c
Thomerlin <thomazzribeiro@hotmail.com>
2021-07-09 16:52:22 -0300

    cry5

--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,9 @@
 
 int			get_next_line(int fd, char **line);
 int			ft_verific_newline(char *str);
-size_t	    ft_strlen(const char *s);
-char	    *ft_strjoin(const char *s1, const char *s2);
+size_t		ft_strlen(const char *s);
+void		*ft_memmove(void *dst, const void *src, size_t len);
+char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_verific_buff(int fd, char **line);
 
 #endif
