8997bc5268a0f86f37db9aaa6e0a03e5fac349bf
Thomerlin <thomazzribeiro@hotmail.com>
2021-07-09 16:08:31 -0300

    agora vai

--- get_next_line.c
+++ get_next_line.c
@@ -1,118 +1,79 @@
 #include "get_next_line.h"
 
-char	*write_beginning(char *buf)
+char	*save_next(char *s)
 {
-	char	*bgn;
-	size_t	len_buf;
 	size_t	i;
+	size_t	count;
+	char	*str;
+
+	i = 0; 
+	count = 0;
+	while (s[i] && s[i] != '\n')
+		i++;
+	if (!s[i])
+	{
+		free(s);
+		return (0);
+	}
+	str = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!str)
+		return (0);
+	i++;
+	while (s[i])
+		str[count++] = s[i++];
+	str[count] = '\0';
+	free(s);
+	return (str);
+}
+
+char *get_line(char *s)
+{
+	int		i;
+	char	*str;
 
 	i = 0;
-	len_buf = ft_strlen(buf, '\n');
-	bgn = malloc(sizeof(char) * (len_buf + 1));
-	if (!bgn)
-		return (NULL);
-	while (i < len_buf)
+	if (!s)
+		return (0);
+	while (s[i] && s[i] != '\n')
+		i++;
+	str = malloc(sizeof(char) * (i + 1));
+	if (!str)
+		return (0);
+	i = 0;	
+	while (s[i] && s[i] != '\n')
 	{
-		bgn[i] = buf[i];
+		str[i] = s[i];
 		i++;
 	}
-	bgn[i] = '\0';
-	return (bgn);
-}
-
-char	*write_end(char **line, char *buf, int *end)
-{
-	char	*bgn;
-	char	*dest;
-	size_t	i;
-
-	i = 0;
-	bgn = write_beginning(buf);
-	dest = ft_strdup(buf);
-	line[i] = ft_strjoin(line[i], bgn);
-	if (!bgn || !dest || !line[i])
-	{
-		free(line[i]);
-		line[i] = NULL;
-		free(buf);
-		return (NULL);
-	}
-	free(bgn);
-	end[i] = 1;
-	return (dest);
-}
-
-int	read_buf(int fd, int *result, char **line, char **buf)
-{
-	size_t	i;
-
-	i = 0;
-	buf[i] = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	result[i] = read(fd, buf[i], BUFFER_SIZE);
-	if (!buf[i] || result[i] == -1)
-	{
-		free(line[i]);
-		line[i] = NULL;
-		free(buf[i]);
-		return (0);
-	}
-	*(buf[i] + result[i]) = '\0';
-	while (!(ft_strchr(buf[i], '\n')) && result[i] > 0)
-	{
-		line[i] = ft_strjoin(line[i], buf[i]);
-		if (!line[i])
-			return (0);
-		result[i] = read(fd, buf[i], BUFFER_SIZE);
-		if (!result[i])
-			return (1);
-		*(buf[i] + result[i]) = '\0';
-	}
-	return (2);
-}
-
-int	process_end(char **line, char **end, int *is_end)
-{
-	size_t	i;
-
-	i = 0;
-	if (ft_strchr(end[i], '\n'))
-	{
-		end[i] = write_end(line, end[i], is_end);
-		if (!end[i])
-			return (0);
-		return (1);
-	}
-	line[i] = ft_strjoin(line[i], end[i]);
-	if (!line[i])
-		return (0);
-	free(end[i]);
-	is_end[i] = 0;
-	return (2);
+	str[i] = '\0';
+	return (str);
 }
 
 int	get_next_line(int fd, char **line)
 {
-	static int	is_end;
-	static char	*end;
-	char		*buf;
-	int			res;
+	static char	*save;
+	char		*buff;
+	int			i;
 
-	if (fd < 0 || BUFFER_SIZE < 1 || !line || !(*line = malloc(sizeof(char))))
+	buff = verific_buff(fd, line);
+	if(!buff)
 		return (-1);
-	**line = '\0';
-	if (is_end)
+	i = 1;
+	while (verific_newline(save) != 1 && i != 0)
 	{
-		if (!(res = process_end(line, &end, &is_end)))
+		i = read(fd, buff, BUFFER_SIZE);
+		if (i == -1)
+		{
+			free(buff);
 			return (-1);
-		if (res == 1)
-			return (1);
+		}
+		buff[i] = '\0';
+		save = ft_strjoin;
 	}
-	if (!(read_buf(fd, &res, line, &buf)) || (ft_strchr(buf, '\n') && !(end = write_end(line, buf, &is_end))))
-		return (-1);
-	if (!res)
-	{
-		free(buf);
+	free(buff);
+	*line = get_line(save);
+	save = save_next(save);
+	if (i == 0)
 		return (0);
-	}
 	return (1);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,63 +1,83 @@
 #include "get_next_line.h"
 
-char	*ft_strchr(const char *s, int c)
+size_t	ft_strlen(const char *s)
 {
-	unsigned char	conv_c;
-
-	conv_c = (unsigned char)c;
-	while (*s)
-	{
-		if (*s == conv_c)
-			return ((char *)s);
-		s++;
-	}
-	if (conv_c == *s)
-		return ((char *)s);
-	return (NULL);
-}
-
-char	*ft_strdup(const char *src)
-{
-	char	*copy;
-	int		size;
-
-	size = ft_strlen(src) + 1;
-	copy = (char *)malloc(size);
-	if (copy != 0)
-	{
-		ft_strlcpy(copy, src, size);
-		return (copy);
-	}
-	return (NULL);
-}
-
-char	*ft_strjoin(const char *s1, const char *s2)
-{
-	size_t	len;
-	size_t	i;
-	char	*result;
-
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-	len = ft_strlen(s1) + ft_strlen(s2);
-	result = malloc(len + 1);
-	if (result == NULL)
-		return (NULL);
-	i = 0;
-	while (*s1)
-		result[i++] = *s1++;
-	while (*s2)
-		result[i++] = *s2++;
-	result[i] = '\0';
-	return (result);
-}
-
-size_t	ft_strlen(const char *s, const char c)
-{
-	size_t i;
+	int	i;
 
 	i = 0;
-	while (s[i] != c)
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
 		i++;
 	return (i);
+}
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char	*d;
+	char	*s;
+
+	d = (char *)dst;
+	s = (char *)src;
+	if (dst == src)
+		return (dst);
+	if (s < d)
+	{
+		while (len--)
+			*(d + len) = *(s + len);
+		return (dst);
+	}
+	while (len--)
+		*d++ = *s++;
+	return (dst);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	stot_len;
+	char	*rtn;
+
+	if (!s1 && !s2)
+		return (0);
+	s1_len = ft_strlen((char *)s1);
+	s2_len = ft_strlen((char *)s2);
+	stot_len = s1_len + s2_len + 1;
+	rtn = malloc(sizeof(char) * stot_len);
+	if (!rtn)
+		return (0);
+	ft_memmove(rtn, s1, s1_len);
+	ft_memmove(rtn + s1_len, s2, s2_len);
+	rtn[stot_len - 1] = '\0';
+	free((char *)s1);
+	return (rtn);
+}
+
+int	verific_newline(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*verific_buff(int fd, char **line)
+{
+	char	*buff;
+
+	if (fd < 0 || !line || BUFFER_SIZE <= 0)
+		return (NULL);
+	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
+		return (NULL);
+	return (buff);
 }
