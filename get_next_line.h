a1ba4960ef2acdf42e87badac66b94cd1b3a86c0
Thomerlin <thomazzribeiro@hotmail.com>
2021-07-09 16:09:43 -0300

    agora vai1

--- get_next_line.h
+++ get_next_line.h
@@ -4,14 +4,11 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-int		get_next_line(int fd, char **line);
-size_t	ft_strlen(const char *s, const char c);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *src);
-char	*ft_strjoin(const char *s1, const char *s2);
-char	*write_beginning(char *buf);
-char	*write_end(char **line, char *buf, int *end);
-int		read_buf(int fd, int *result, char **line, char **buf);
-int		process_end(char **line, char **end, int *is_end);
+int			get_next_line(int fd, char **line);
+int			verific_newline(char *str);
+size_t		ft_strlen(const char *str);
+void		*ft_memmove(void *dst, const void *src, size_t len);
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*verific_buff(int fd, char **line);
 
 #endif
