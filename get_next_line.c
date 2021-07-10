1a1489a825ea9adc145a91bdb9ab2f2fe3e998a5
Thomerlin <thomazzribeiro@hotmail.com>
2021-07-10 01:32:23 -0300

    come'n again

--- get_next_line.c
+++ get_next_line.c
@@ -8,9 +8,12 @@ char	*strings_line(char *s)
 	i = 0;
 	if (!s)
 		return (0);
+	while (s[i] && s[i] != '\n')
+		i++;
 	str = malloc(sizeof(char) * (i + 1));
 	if (!str)
 		return (0);
+	i = 0;
 	while (s[i] && s[i] != '\n')
 	{
 		str[i] = s[i];
