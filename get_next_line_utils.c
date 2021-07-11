8904866a04cb15ac246741046fa26a3a0d54ed51
Thomerlin <thomazzribeiro@hotmail.com>
2021-07-11 00:20:14 -0300

    teste

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -35,24 +35,23 @@ void	*ft_memmove(void *dest, const void *src, size_t length)
 	return (dest);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(const char *s1, const char *s2)
 {
-	size_t	len_s1;
-	size_t	len_s2;
-	size_t	lens_total;
-	char	*ret;
+	size_t	len;
+	size_t	i;
+	char	*result;
 
-	if (!s1 && !s2)
-		return (0);
-	len_s1 = ft_strlen((char *)s1);
-	len_s2 = ft_strlen((char *)s2);
-	lens_total = len_s1 + len_s2 + 1;
-	ret = malloc(sizeof(char) * lens_total);
-	if (!ret)
-		return (0);
-	ft_memmove(ret, s1, len_s1);
-	ft_memmove(ret + len_s1, s2, len_s2);
-	ret[lens_total - 1] = '\0';
-	free((char *)s1);
-	return (ret);
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	result = malloc(len + 1);
+	if (result == NULL)
+		return (NULL);
+	i = 0;
+	while (*s1)
+		result[i++] = *s1++;
+	while (*s2)
+		result[i++] = *s2++;
+	result[i] = '\0';
+	return (result);
 }
