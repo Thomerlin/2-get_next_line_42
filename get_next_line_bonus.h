0d64170c0b65ea6684b83d75489198e6712438a8
Thomerlin <thomazzribeiro@hotmail.com>
2021-07-12 14:48:19 -0300

    OK

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,9 +1,10 @@
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
+# define MAX 256
+
 # include <unistd.h>
 # include <stdlib.h>
-# define MAX 256
 
 int		get_next_line(int fd, char **line);
 size_t	ft_strlen(const char *s);
